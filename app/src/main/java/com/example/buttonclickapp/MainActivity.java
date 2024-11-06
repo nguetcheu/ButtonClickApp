9977147de7cc4e28c26e4a8f6c04ab6c3f75ad93
nguetcheu <dnguetcheu@gmail.com>
2024-11-06 12:57:12 +0100

    Récuperation du contenu saisie par utilisateur dans un editText avant le click sur le bouton pour son affichage

--- app/src/main/java/com/example/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.buttonclickapp;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        userInput = (EditText) findViewById(R.id.editTextText);
+        Button button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView.setText("");
+        textView.setMovementMethod(new ScrollingMovementMethod());
+        View.OnClickListener ourOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String result = userInput.getText().toString();
+                result = result + "\n";
+                textView.append(result);
+            }
+        };
+        if(button != null) {
+            button.setOnClickListener(ourOnClickListener);
+        }
+
     }
 }
